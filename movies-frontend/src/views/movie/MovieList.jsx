2e2f4bbb8017c0589414c64d40f97c0027e4ff0b
Azka Zaki <azkazaki11@gmail.com>
2024-01-21 18:29:46 +0700

    Edit Movie List

--- movies-frontend/src/views/movie/MovieList.jsx
+++ movies-frontend/src/views/movie/MovieList.jsx
@@ -27,7 +27,7 @@ const MovieList = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 rounded  ">
           {movies.map((movie) => (
             <Link to={`/movies/${movie.id}`} key={movie.id}>
-              <div key={movie.id} className=" rounded  hover:shadow-lg hover:shadow-slate-50 box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 50px -12px; ">
+              <div key={movie.id} className=" rounded hover:scale-110 transform transition-transform duration-300 ease-in-out hover:shadow-lg hover:shadow-slate-50 box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 50px -12px; ">
                 <div className="aspect-w-2 aspect-h-3">
                   <img src={movie.poster} alt={movie.title} className="object-cover rounded-t " />
                 </div>
