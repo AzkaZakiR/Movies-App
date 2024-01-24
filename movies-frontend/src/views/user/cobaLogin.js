ae3d18081e3c1beb9adf2cef94c5e4e58ccd72c7
aliazhar <aliazhar.idx@gmail.com>
2024-01-24 19:49:30 +0700

    [ ^ ] Fix login: enter to submit

--- movies-frontend/src/views/user/cobaLogin.js
+++ movies-frontend/src/views/user/cobaLogin.js
@@ -49,34 +49,38 @@ function Modal({ closeModal, openRegister }) {
         <div className="title mb-6">
           <h1 className="text-2xl font-bold text-red-700 ">LOG IN</h1>
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Username</label>
-          <input
-            className="shadow rounded-md appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter your username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          {error && !error.includes("Wrong Password") && <p className="text-red-500 mt-1">{error}</p>}
-        </div>
-        <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-            Password
-          </label>
-          <input
-            className="shadow rounded-md appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="password"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {error && !error.includes("user not found") && <p className="text-red-500 mt-1">{error}</p>}
-        </div>
-        <div className="flex items-center justify-between">
-          <button className="border border-red-500 bg-white text-red-500 py-2 px-4 rounded-md w-full hover:bg-red-500 hover:text-white transition-colors duration-700" type="submit" onClick={handleLogin}>
-            Login
-          </button>
-        </div>
+
+        <form onSubmit={handleLogin}>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Username</label>
+            <input
+              className="shadow rounded-md appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Enter your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            {error && !error.includes("Wrong Password") && <p className="text-red-500 mt-1">{error}</p>}
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="shadow rounded-md appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {error && !error.includes("user not found") && <p className="text-red-500 mt-1">{error}</p>}
+          </div>
+          <div className="flex items-center justify-between">
+            <button className="border border-red-500 bg-white text-red-500 py-2 px-4 rounded-md w-full hover:bg-red-500 hover:text-white transition-colors duration-700" type="submit">
+              Login
+            </button>
+          </div>
+        </form>
+
         <div className="text-center mt-4">
           <h1 className="text-center inline-block font-bold text-sm text-blue-500 hover:text-blue-800">
             New to SEA CINEMA?{" "}
