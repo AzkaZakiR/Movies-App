d82197cbec2c6c3365ac6881cbe5f80716f400ea
aliazhar <aliazhar.idx@gmail.com>
2024-01-24 20:04:18 +0700

    [ ^ ] Update X (not twitter :v) icon in Login and Register Page

--- movies-frontend/src/views/user/Register.js
+++ movies-frontend/src/views/user/Register.js
@@ -47,7 +47,7 @@ function RegisterModal({ closeModal, openLogin }) {
             }}
             className="text-gray-600 text-2xl"
           >
-            X
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
           </button>
         </div>
         <h2 className="text-2xl  font-bold my-6 text-red-700">Register</h2>
--- movies-frontend/src/views/user/cobaLogin.js
+++ movies-frontend/src/views/user/cobaLogin.js
@@ -45,7 +45,7 @@ function Modal({ closeModal, openRegister }) {
             }}
             className="text-gray-600 text-2xl"
           >
-            X
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
           </button>
         </div>
         <div className="title mb-6">
